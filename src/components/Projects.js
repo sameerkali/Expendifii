32a561c92b0dc4eb2dc288ae19b9a9b8c561cc36
sameerkali <s.faridi007@gmail.com>
2024-07-16 09:52:08 +0530

    add: loadmore in project card

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,8 @@
+"use client"
 import Link from "next/link";
 import Image from "next/image";
+import { projectsData } from '../app/utils'
+import { useEffect, useState } from "react";
 
 const ProjectCard = ({ imgSrc, name, description, liveLink }) => {
   return (
@@ -19,110 +22,34 @@ const ProjectCard = ({ imgSrc, name, description, liveLink }) => {
 };
 
 const Projects = () => {
-  const projectsData = [
-    {
-      imgSrc: "/projects/1.png",
-      name: "Walk State",
-      description: "The largest real estate hub for home owners, buyers and agents.",
-      liveLink: "https://walkestate.com/"
-    },
-    {
-      imgSrc: "/projects/2.png",
-      name: "Sport Skpi",
-      description: "To providing cutting-edge sports analytics solutions that empower our clients with data-driven insights and strategies.",
-      liveLink: "https://www.sportskpi.com/"
-    },
-    {
-      imgSrc: "/projects/3.png",
-      name: "Royal Floors",
-      description: "Our extensive selection includes Australian hardwood, durable laminate, stylish vinyl, plush carpet, and sleek tile materials. Explore the variety of flooring solutions offered by Royal Floors to find the perfect fit for your space.",
-      liveLink: "https://royalfloors.com.au/"
-    },
-    {
-      imgSrc: "/projects/4.png",
-      name: "Owltics",
-      description: "Engineering teams benefit from hassle-free tracking. Capture every step seamlessly with simple log tracking.",
-      liveLink: "https://owltics.com/"
-    },
-    {
-      imgSrc: "/projects/5.png",
-      name: "Mybhtc",
-      description: "At MyBHTC, our team of providers believe getting in touch via tele-health is an essential part of raising up our community that needs us. If you are an existing client, your sessions will be via tele-health until further notice.",
-      liveLink: "https://mybhtc.com/"
-    },
-    {
-      imgSrc: "/projects/6.png",
-      name: "Trusurant",
-      description: "Choose Trusurant Today for more secure Tomorrow Your trusted partner in uncovering the truth. With a reputation for excellence and confidentiality, we specialize in providing comprehensive investigative services to individuals, businesses, and organizations worldwide.",
-      liveLink: "https://www.trusurant.com/"
-    },
-    {
-      imgSrc: "/projects/7.png",
-      name: "Globaltechnoit",
-      description: "We help businesses elevate their value through custom software development, product design, QA and consultancy services.",
-      liveLink: "https://globaltechnoit.com/"
-    },
-    {
-      imgSrc: "/projects/8.png",
-      name: "Skesbadami",
-      description: "Educational institutions are vital to the development of knowledge, skills and academic qualifications, and as a Secretary of such an organization.",
-      liveLink: "https://skesbadami.org/"
-    },
-    {
-      imgSrc: "/projects/9.png",
-      name: "Acceltech",
-      description: "At MyBHTC, our team of providers believe getting in touch via tele-health is an essential part advanced coding skills.",
-      liveLink: "https://acceltech.in/"
-    },
-    {
-      imgSrc: "/projects/10.png",
-      name: "Lal Sweets",
-      description: "Lal Sweets (P) Ltd is a new age Mithai company headquartered in Bengaluru. Started in 2010 with a vision of selling Mithai (Indian dessert) as a packaged product in the FMCG food category. From humble beginnings, our hard work and dedication have been instrumental in bringing this organization to its current form of fame and glory.",
-      liveLink: "https://www.lalsweets.com/"
-    },
-    {
-      imgSrc: "/projects/11.png",
-      name: "Oyebonge",
-      description: "We are a new-age clothing store for teenagers, bringing you the latest trends and styles from around the world.",
-      liveLink: "https://www.oyebonge.com/"
-    },
-    {
-      imgSrc: "/projects/12.png",
-      name: "Plutope",
-      description: "From product to a full ecosystem, we've built an incredible team of experienced people that share our passion for disruptive technology and vision of a new digital payments landscape.",
-      liveLink: "https://www.plutope.io/"
-    },
-    {
-      imgSrc: "/projects/13.png",
-      name: "Green Feels",
-      description: "First things first, let me introduce myself. I'm Priya, a sustainability enthusiast and advocate on a mission to change our habits, one plastic-free swap at a time. But, hey, let's face it - we've all been there, right? Mindless consumers, blindly accepting whatever was thrown our way, without ever questioning its impact. I used to be just like that, but one fateful day, I had an epiphany - a moment of truth that changed everything.",
-      liveLink: "https://greenfeels.in/"
-    },
-    {
-      imgSrc: "/projects/14.png",
-      name: "Aphiliac",
-      description: "An avant-garde streetwear clothing brand, is poised to evolve the Indian fashion trend with its stylish and original designs, especially for Gen Z. Launching on January 1, 2024. Founded by the insightful designer Rajvansh Devgan, AKA Richie, Aphiliac Apparel is more than a clothing brand; it manifests individuality and rebellion against the commonplace.",
-      liveLink: "https://aphiliac.store/"
-    },
-    {
-      imgSrc: "/projects/15.png",
-      name: "CAKNJ",
-      description: "We are a firm of Chartered Accountants established in the year 1993, registered with The Institute of Chartered Accountants of India. We provide comprehensive professional services including accounting, financial and legal consulting, audit, management consultancy, tax consultancy, accounting services, manpower management, secretarial services, financial accounting consultants, payroll management, and other financial services.",
-      liveLink: "https://www.caknj.com/"
-    },
-    {
-      imgSrc: "/projects/16.png",
-      name: "Prabhubhakti",
-      description: "At Prabhubhakti, we are on a mission to bridge the gap between tradition and modernity by crafting unique spiritual products tailored for the youth. Our journey began with a profound belief that spirituality should not only be a timeless tradition but also an exciting and dynamic experience for the younger generation.",
-      liveLink: "https://prabhubhakti.com/"
-    },
-    {
-      imgSrc: "/projects/17.png",
-      name: "My Farmer Friend",
-      description: "Our vision is to bring the concept of “KEEP YOUR FRIENDS CLOSE, YOUR FARMERS CLOSER” to life in the near future.",
-      liveLink: "https://myfarmerfriend.com"
-    },
-  ];
+  const [currentPage, setCurrentPage] = useState(1);
+  const [cardsPerPage, setCardsPerPage] = useState(8);
+  const [projects, setProjects] = useState([]);
+
+  const handleLoadMore = () => {
+    const startIndex = currentPage * cardsPerPage;
+    const endIndex = startIndex + cardsPerPage;
+    const newProjects = projectsData.slice(startIndex, endIndex);
+    setProjects([...projects, ...newProjects]);
+    setCurrentPage(currentPage + 1);
+  };
+
+  useEffect(() => {
+    const updateCardsPerPage = () => {
+      const mediaQuery = window.matchMedia("(max-width: 768px)");
+      setCardsPerPage(mediaQuery.matches ? 4 : 8);
+    };
+
+    updateCardsPerPage();
+
+    window.addEventListener("resize", updateCardsPerPage);
+    return () => window.removeEventListener("resize", updateCardsPerPage);
+  }, []);
+
+  useEffect(() => {
+    const initialProjects = projectsData.slice(0, cardsPerPage);
+    setProjects(initialProjects);
+  }, [cardsPerPage]);
 
   return (
     <section className="relative container mx-auto px-5 md:px-16 flex flex-col gap-5" id="projects">
@@ -131,7 +58,7 @@ const Projects = () => {
         <h2 className="title text-center">Our Recent Work</h2>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {projectsData.map((project, index) => (
+        {projects.map((project, index) => (
           <ProjectCard
             key={index}
             imgSrc={project.imgSrc}
@@ -141,6 +68,11 @@ const Projects = () => {
           />
         ))}
       </div>
+      {projects.length < projectsData.length && (
+        <button onClick={handleLoadMore} className="bg-yellow-400 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded">
+          Load More
+        </button>
+      )}
     </section>
   );
 };
