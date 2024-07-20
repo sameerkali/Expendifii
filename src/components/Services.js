95e7645a1663f7a77515ab22b116384ec94dd562
Sameer Faridi <s.faridi007@gmail.com>
2024-07-20 23:32:15 +0530

    remove yt button

--- src/components/Services.js
+++ src/components/Services.js
@@ -35,10 +35,7 @@ const Services = () => {
             className="object-cover w-full"
           />
           <button className="w-32 h-32 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            {/* <Link href={"https://youtu.be/aVNa8Dka5MA"} target="_blank"> */}
-              <span className="playButton duration-700 w-full h-full bg-rose-500 rounded-full block absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></span>
-              {/* <PlayArrowIcon className="w-16 h-16 md:w-24 md:h-24 text-white bg-[#ffffffb8] rounded-full" /> */}
-            {/* </Link> */}
+          
           </button>
         </div>
         <div className="flex gap-5 md:gap-10 flex-col flex-1">
