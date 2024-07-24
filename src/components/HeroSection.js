79a53c62b47df15874d23039d278573d2a9e3e71
sameerkali <s.faridi007@gmail.com>
2024-07-24 16:48:15 +0530

    add: click to asd

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,12 +1,28 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
+import {useState} from 'react'
 
 const HeroSection = () => {
+  const [num, setNum] = useState(0)
+  const handleClick = () => {
+    setNum(s => s+1) // 
+    console.log(num)
+    setNum(s => s+1)
+    console.log(num)
+    setNum(s => s+1)
+    console.log(num)
+  }
   return (
     <section
       className="text-center flex flex-col gap-10 sm:gap-20 items-center justify-center h-full mt-28 sm:mt-32 md:mt-44"
       id="home"
     >
+      <div>
+        <h1>{num}</h1>
+        <button onClick={handleClick}>click me</button>
+      </div>
       <div className="md:w-2/3 lg:w-1/2 container px-5 md:px-16 mx-auto">
         <h1 className="capitalize flex flex-col gap-2 md:gap-5 text-3xl sm:text-4xl md:text-[3.2rem] 2xl:text-6xl font-bold">
           <p>top quality digital </p>
@@ -17,9 +33,7 @@ const HeroSection = () => {
           and app development solutions tailored to your business needs ever.
         </p>
         <button className="md:text-base text-sm hover:border-2 border-2 border-transparent font-semibold py-3 px-8 md:px-10 text-white bg-rose-600 hover:border-rose-600 hover:bg-transparent hover:text-rose-600 rounded-full">
-          <Link href="#projects" >
-            Explore
-          </Link>
+          <Link href="#projects">Explore</Link>
         </button>
       </div>
       <div className="w-full relative">
